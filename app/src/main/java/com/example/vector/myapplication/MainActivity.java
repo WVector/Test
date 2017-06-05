51314bc5c7a6078c231ff833ace122ca9f075f1a
vector <1964414877@qq>
2017-06-05 21:21:05 +0800

    首次提交

--- app/src/main/java/com/example/vector/myapplication/MainActivity.java
+++ app/src/main/java/com/example/vector/myapplication/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.vector.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = new TextView(this);
     }
 }
